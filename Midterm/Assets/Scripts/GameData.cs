ba30da9aac820524cfc913b76100b8a79b3be89a
Gabriel <55121175+Vannerss@users.noreply.github.com>
2022-10-11 18:56:51 -0500

    lower section of map
    Co-Authored-By: NikoRios815 <58575679+NikoRios815@users.noreply.github.com>

---
+++ Midterm/Assets/Scripts/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public Vector3 cameraPosition;
+    public Vector3 cameraRotation;
+
+
+}
